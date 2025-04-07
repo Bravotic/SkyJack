c3245c2695527c6f0685ca033132711d5c480bb6
AC-Dc <changdavidson4@gmail.com>
2025-04-07 09:56:12 -0400

    borrowing

---
+++ examples/borrow.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(require "../lib/spec.rkt" "../lib/text-generator.rkt"
+         "../lib/runtime-data.rkt" "../lib/runtime-functions.rkt")
+(require "./plan_reuse.rkt")
+
+(define-plan short-flight
+  boston-north-departure
+  D-> AUG
+  D-> KBGR)
